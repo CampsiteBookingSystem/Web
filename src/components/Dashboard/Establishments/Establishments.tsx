baa09d544f1c9dcc98fbabdfd3c393b76a6d8303
Bram van Osta <bramvanosta@gmail.com>
2019-10-20 16:28:06 +0200

    feature: Redirect to establishment if only one is available

--- src/components/Dashboard/Establishments/Establishments.tsx
+++ src/components/Dashboard/Establishments/Establishments.tsx
@@ -6,9 +6,14 @@ import { State } from '../../../store';
 import Establishment from './Establishment';
 
 import './Establishments.css';
+import { Redirect } from 'react-router';
 
 function Establishments() {
   const { establishments } = useSelector((state: State) => state.establishment);
+
+  if (establishments.length === 1) {
+    return <Redirect to={`/${establishments[0].slug}`} />;
+  }
 
   return (
     <div className="Establishments">
